52d900a4157c6c56f7a9fe2ef2d35eebc8562710
Jem Cope <thejemmeh@gmail.com>
2022-05-10 14:00:01 -0700

    Section 1 Car Scene Complete

---
+++ Car/Car Experiment/Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Beep beep traffic is at a standstill because our tires dont roll.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
