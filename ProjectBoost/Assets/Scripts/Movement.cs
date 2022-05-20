857e440b38bced085cd379418185474245c01c34
Jem Cope <thejemmeh@gmail.com>
2022-05-19 23:43:24 -0700

    Project boost. finished AddRelativeForce

--- ProjectBoost/Assets/Scripts/Movement.cs
+++ ProjectBoost/Assets/Scripts/Movement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessThrust();
+        ProcessRotation();
     }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+    }
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed Left-A");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed Right - D");
+        }
+    }
+
+   
 }
