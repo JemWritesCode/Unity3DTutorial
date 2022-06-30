85273586bdbeeb69d5cb81461952ebc7f0dc26b8
Jem Cope <thejemmeh@gmail.com>
2022-06-30 16:28:58 -0700

    Scoreboard updates as you kill enemies

--- ArgonAssault/Assets/Scripts/Scoreboard.cs
+++ ArgonAssault/Assets/Scripts/Scoreboard.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
     public int score;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "0";
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
+        scoreText.text = score.ToString();
     }
 }
