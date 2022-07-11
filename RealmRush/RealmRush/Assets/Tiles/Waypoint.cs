f6a38eed6b36876c2c7a3e08640fc09138571a70
Jem Cope <thejemmeh@gmail.com>
2022-07-11 01:02:52 -0700

    118 Complete can click on tiles to get back the coords in console. prep for placing tiles only on grass not roads

--- RealmRush/RealmRush/Assets/Tiles/Waypoint.cs
+++ RealmRush/RealmRush/Assets/Tiles/Waypoint.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    
+    [SerializeField] bool isPlacable;
+
+    private void OnMouseDown()
+    {
+        if (isPlacable)
+        {
+            Debug.Log(transform.name);
+        }
+    }
 }
