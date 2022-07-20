c2603e0eccb9bfa1fd098f82d7647663408dbc36
Jem Cope <thejemmeh@gmail.com>
2022-07-20 11:13:04 -0700

    127. Towers now cost money and you can't build them if you're out of money. Level resets if you go negative on money.

--- RealmRush/RealmRush/Assets/Bank/Bank.cs
+++ RealmRush/RealmRush/Assets/Bank/Bank.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
@@ -20,5 +21,16 @@ public class Bank : MonoBehaviour
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        if (currentBalance < 0)
+        {
+            //lose the game
+            ReloadScene();
+        }
+    }
+
+    void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
