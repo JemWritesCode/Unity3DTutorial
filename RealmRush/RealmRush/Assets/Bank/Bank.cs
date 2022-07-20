165f5c76b417d63ee9c508a0713eddae514b0400
Jem Cope <thejemmeh@gmail.com>
2022-07-20 11:52:18 -0700

    128 Setup UI for the gold balance

--- RealmRush/RealmRush/Assets/Bank/Bank.cs
+++ RealmRush/RealmRush/Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,23 +10,34 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
+
         if (currentBalance < 0)
         {
             //lose the game
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void ReloadScene()
