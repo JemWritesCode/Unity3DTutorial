26b7cde1f6e38e8c5e5a8278b0c171c69b3248de
Jem Cope <thejemmeh@gmail.com>
2022-07-24 17:43:44 -0700

    136 Started a dictionary to hold the nodes for pathing

--- RealmRush/RealmRushWithAlgoPathfinding/Assets/Pathfinding/GridManager.cs
+++ RealmRush/RealmRushWithAlgoPathfinding/Assets/Pathfinding/GridManager.cs
@@ -1,21 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Node node;
+    [SerializeField] Vector2Int gridSize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        Debug.Log(node.coordinates);
-        Debug.Log(node.isWalkable);
+        CreateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateGrid()
     {
-        
+        for(int x = 0; x < gridSize.x; x++)
+        {
+            for(int y = 0; y < gridSize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x,y);
+                grid.Add(coordinates, new Node(coordinates, true));
+                Debug.Log(grid[coordinates].coordinates + " = " + grid[coordinates].isWalkable);
+            }
+        }
     }
 }
