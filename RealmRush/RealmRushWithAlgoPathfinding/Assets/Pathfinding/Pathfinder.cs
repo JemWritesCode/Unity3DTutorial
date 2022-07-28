f4d01b0bd12656953244d7cf1552f5f1ebf7750c
Jem Cope <thejemmeh@gmail.com>
2022-07-27 23:44:43 -0700

    140 Breadth First Search pathfinding is setup and applied to the coordinate labeler. It can build a path to travel now

--- RealmRush/RealmRushWithAlgoPathfinding/Assets/Pathfinding/Pathfinder.cs
+++ RealmRush/RealmRushWithAlgoPathfinding/Assets/Pathfinding/Pathfinder.cs
@@ -26,15 +26,15 @@ public class Pathfinder : MonoBehaviour
         {
             grid = gridManager.Grid;
         }
-
-        startNode = new Node(startCoordinates, true);
-        destinationNode = new Node(destinationCoordinates, true);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        startNode = gridManager.Grid[startCoordinates];
+        destinationNode = gridManager.Grid[destinationCoordinates];
         BreadthFirstSearch();
+        BuildPath();
     }
 
     private void ExploreNeighbors()
@@ -53,6 +53,7 @@ public class Pathfinder : MonoBehaviour
         {
             if(!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable)
             {
+                neighbor.connectedTo = currentSearchNode;
                 reached.Add(neighbor.coordinates, neighbor);
                 frontier.Enqueue(neighbor);
             }
@@ -76,5 +77,23 @@ public class Pathfinder : MonoBehaviour
                 isRunning = false;
             }
         }
+    }
+
+    List<Node> BuildPath()
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = destinationNode;
+
+        path.Add(currentNode);
+        currentNode.isPath = true;
+
+        while(currentNode.connectedTo != null)
+        {
+            currentNode = currentNode.connectedTo;
+            path.Add(currentNode);
+            currentNode.isPath = true;
+        }
+        path.Reverse();
+        return path;
     }
 }
