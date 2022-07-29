d8ddbfb0ba81859088a59cc05dd412021406dae0
Jem Cope <thejemmeh@gmail.com>
2022-07-28 23:59:10 -0700

    139 working on blocking nodes

--- RealmRush/RealmRushWithAlgoPathfinding/Assets/Pathfinding/GridManager.cs
+++ RealmRush/RealmRushWithAlgoPathfinding/Assets/Pathfinding/GridManager.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] Vector2Int gridSize;
+
+    [Tooltip("World Grid Size - Should match UnityEditor snap settings.")]
+    [SerializeField] int worldGridSize;
     Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
     public Dictionary<Vector2Int, Node> Grid { get { return grid; } }
 
@@ -22,6 +25,24 @@ public class GridManager : MonoBehaviour
         }
         return null;
     }
+
+    public void BlockNode(Vector2Int coordinates)
+    {
+        if (grid.ContainsKey(coordinates))
+        {
+            grid[coordinates].isWalkable = false;
+        }
+    }
+
+    public Vector2Int GetCoordinatesFromPosition(Vector3 position)
+    {
+        Vector2Int coordinates = new Vector2Int();
+        coordinates.x = Mathf.RoundToInt(position.x / worldGridSize);
+        coordinates.y = Mathf.RoundToInt(position.z / UnityEditor.EditorSnapSettings.move.z);
+
+    }
+
+
     private void CreateGrid()
     {
         for(int x = 0; x < gridSize.x; x++)
